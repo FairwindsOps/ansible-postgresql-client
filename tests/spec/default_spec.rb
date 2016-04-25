5e52279c66c641ff5744aa8daa74da6ea68cab13
Justin Mound <justinmound@gmail.com>
2016-04-25 15:50:33 -0400

    additional tests

--- tests/spec/default_spec.rb
+++ tests/spec/default_spec.rb
@@ -8,6 +8,10 @@ describe file('/usr/share/postgresql-common/pg_wrapper') do
   it { should be_mode 755 }
 end
 
-describe package('postgresql-9.4') do
+describe package('postgresql-client-9.4') do
   it { should be_installed }
+end
+
+describe command('psql --version') do
+  its(:stdout) { should match /9.4.7/ }
 end
